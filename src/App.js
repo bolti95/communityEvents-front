5c0ab539f7a8ce6cca2adbcbb1eeab2edf554e4b
bolti95 <izzybolt95@gmail.com>
2022-01-19 16:14:28 +0000

    commiting to change branch

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import './App.css';
 import Page from './components/Page';
 import { Routes, Route, BrowserRouter} from 'react-router-dom'
 import EventForm from './components/forms/EventForm';
+import { PageDefault } from './styles/Page';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Community Map</h1>
+      <PageDefault flexDirect={"column"} display={"flex"}>
+        <header>
+        <h1>Community Map</h1>
+        </header>
+      </PageDefault>
       <BrowserRouter>
       <Routes>
         <Route exact path= "/" element={<Page />} />
         <Route exact path= "/newevent" element={<EventForm />} />
       </Routes>
       </BrowserRouter>
-      <Page />
     </div>
   );
 }
